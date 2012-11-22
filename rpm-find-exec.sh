b6930ff53cc8d31b384cc0fdae32fb97bec954f7
Grant Murphy <grantcmurphy@gmail.com>
2012-11-22 22:15:44 +1000

    Update rpm-find-exec.sh

--- rpm-find-exec.sh
+++ rpm-find-exec.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# Produces a list of all setuid executables installed on a 
-# distribution and checks if they have been built using PIE. 
+# Produces a csv list of all executable files and the packages that they belong to 
+# including the details of whether they have been built using security hardened
+# options.
 
 for file in "$@"; do
     for data in `rpm -qp --queryformat '[%{NAME} %{FILEMODES:perms} %{FILENAMES}\n]' $file | grep -E "^.* -..x..x..x " | awk '{ print sprintf("%s:%s", $1, $3)}'`; do
 
-	    split=`echo $data | sed -e 's/\:/ /'`
-	    set -- $split
+	split=`echo $data | sed -e 's/\:/ /'`
+	set -- $split
         echo $2 | grep -E "/lib.*" >> /dev/null
         is_lib=$? 
         echo $2 | grep -E "/usr/share.*" >> /dev/null
         is_shared=$?
-
-	    if  test $is_shared -eq 1 && test $is_lib -eq 1 && test -x $2 && test -f $2; then 
+        if  test $is_shared -eq 1 && test $is_lib -eq 1 && test -x $2 && test -f $2; then 
             rv=`rpm2cpio $file | cpio --to-stdout -iv .$2 2>/dev/null | ./checksec`
             if test $? -eq 0; then 
                 echo $(basename $file),$2,$rv
             fi
-	    fi
+        fi
     done
 done
